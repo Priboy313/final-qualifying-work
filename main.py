ef2bfed508741c0f1d635ce07b63c00462475145
Priboy313 <Priboy313@yandex.ru>
2024-12-21 20:25:54 +0300

    fix main.py

--- main.py
+++ main.py
@@ -53,24 +53,8 @@ i_ind = 10
 
 threshold = 0.13
 
-def main():
 
-    model_R1L1 = CatBoostRegressor().load_model(f"Model/R1L1_{R1L1}.cbm")
-    model_R2L1 = CatBoostRegressor().load_model(f"Model/R2L1_{R2L1}.cbm")
-    model_C1L2 = CatBoostClassifier().load_model(f"Model/C1L2_{C1L2}.cbm")
-    model_C2L2 = CatBoostClassifier().load_model(f"Model/C2L2_{C2L2}.cbm")
-
-
-    df = pd.read_csv('Data/EURUSD_H1_2015-01-21_2024-10-31.zip')
-    df = df.tail(168)
-    df.columns = ['date', 
-                  'open', 
-                  'high', 
-                  'low', 
-                  'close', 
-                  'volume'
-                  ]
-
+def preprocessing(df:pd.DataFrame) -> pd.DataFrame:
     df['datetime'] = pd.to_datetime(df['date'])
     df.index = pd.to_datetime(df['date'])
     df[['date', 'time']] = df['date'].str.split(' ', expand=True)[[0, 1]]
@@ -141,8 +125,9 @@ def main():
     df.loc[:, 'y_true'] = (df.loc[:, 'close'] < df.loc[:, 'y_close']) * 1
     df.loc[:, 'y_true_down'] = (df.loc[:, 'close'] > df.loc[:, 'y_close']) * 1
 
-    df = df.dropna()
+    return df.dropna()
 
+def get_lists(df:pd.DataFrame) -> list:
     list_base_time = ['time', 'weekday', 'daypart']
     list_base = ['open_change',
         'close_change', 'high_change', 'low_change',
@@ -170,16 +155,35 @@ def main():
     for c1 in list_cat_feat:
         for c2 in list(df.columns):
             if c1 in c2: cat_features.append(c2)
+    
+    return X1L1, X2L1, XL2, cat_features
 
-    X1_cat_features = []
-    for c1 in list_cat_feat:
-        for c2 in list(set(df.columns) & set(X1L1)):
-            if c1 in c2: X1_cat_features.append(c2)
+def get_inds(df:pd.DataFrame) -> pd.DataFrame:
+    df['ind_up'] = ((df['pred_y_up'] - df['pred_y_down']) > threshold).astype(float)
+    df.loc[df['ind_up'] != 1, 'ind_up'] = float("NaN")
+    df.loc[df['ind_up'] == 1, 'ind_up'] = df['low'] - 0.0003
 
-    X2_cat_features = []
-    for c1 in list_cat_feat:
-        for c2 in list(set(df.columns) & set(X2L1)):
-            if c1 in c2: X2_cat_features.append(c2)
+    df['ind_down'] = ((df['pred_y_down'] - df['pred_y_up']) > threshold).astype(float)
+    df.loc[df['ind_down'] != 1, 'ind_down'] = float("NaN")
+    df.loc[df['ind_down'] == 1, 'ind_down'] = df['high'] + 0.0003
+
+    return df
+
+def main():
+
+    model_R1L1 = CatBoostRegressor().load_model(f"Model/R1L1_{R1L1}.cbm")
+    model_R2L1 = CatBoostRegressor().load_model(f"Model/R2L1_{R2L1}.cbm")
+    model_C1L2 = CatBoostClassifier().load_model(f"Model/C1L2_{C1L2}.cbm")
+    model_C2L2 = CatBoostClassifier().load_model(f"Model/C2L2_{C2L2}.cbm")
+
+
+    df = pd.read_csv('Data/EURUSD_H1_2015-01-21_2024-10-31.zip')
+    df = df.tail(168)
+    df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+
+    df = preprocessing(df)
+
+    X1L1, X2L1, XL2, cat_features = get_lists(df)
     
     def asint(x): return x.astype(int) if x.dtype == float else x
     df[cat_features]  = df[cat_features].apply(asint, axis=0)
@@ -190,25 +194,10 @@ def main():
     df.loc[:, 'predict_R2L1']     = model_R2L1.predict(df[X2L1])
     df.loc[:, 'predict_cls_R2L1'] = (df.loc[:,'close'] < df.loc[:,'predict_R2L1']) * 1
 
-
-    cat_features = []
-    for c1 in list_cat_feat:
-        for c2 in list(XL2):
-            if c1 in c2: 
-                cat_features.append(c2)
-
     df.loc[:, 'pred_y_up']      = model_C1L2.predict_proba(df[XL2])[:,1]
     df.loc[:, 'pred_y_down']    = model_C2L2.predict_proba(df[XL2])[:,1]
 
-    df['ind_up'] = ((df['pred_y_up'] - df['pred_y_down']) > threshold).astype(float)
-    df.loc[df['ind_up'] != 1, 'ind_up'] = float("NaN")
-    df.loc[df['ind_up'] == 1, 'ind_up'] = df['low'] - 0.0003
-
-    df['ind_down'] = ((df['pred_y_down'] - df['pred_y_up']) > threshold).astype(float)
-    df.loc[df['ind_down'] != 1, 'ind_down'] = float("NaN")
-    df.loc[df['ind_down'] == 1, 'ind_down'] = df['high'] + 0.0003
-
-
+    df = get_inds(df)
 
     df.index = pd.to_datetime(df['date'] + " " + df['time'])
     get_graph(df.tail(PLOT_WINDOW_S * PLOT_WINDOW_X), volume=True)
